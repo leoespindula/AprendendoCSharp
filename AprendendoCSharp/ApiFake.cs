78f077035cfd55688d0ce6250213dd37b1c6b4ab
leoespindula <espindulasystem.dev@gmail.com>
2022-08-18 17:56:27 -0300

    Initial commit

---
+++ AprendendoCSharp/ApiFake.cs
@@ -0,0 +1,8 @@
+﻿public partial class ApiFake
+{
+    public long PostId { get; set; }
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Body { get; set; }
+}
---
+++ AprendendoCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AprendendoCSharp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var testeApi = await client.GetAsync("https://jsonplaceholder.typicode.com/comments");
+            var consumoApi = await testeApi.Content.ReadAsStringAsync();
+
+            var users = JsonConvert.DeserializeObject<ApiFake[] > (consumoApi);
+
+            foreach (var item in users)
+            {
+                Console.WriteLine(item.Id);
+            }
+        }
+    }
+}
